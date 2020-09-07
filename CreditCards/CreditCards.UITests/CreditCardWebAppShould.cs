a634eb3d12c4822ed04ad2cc96eb83a606313339
Bilal Taherkheli <bilal@danfoss.com>
2020-09-07 14:21:50 +0200

    mod4 end. Assert on page loads after navigating back n forth

--- CreditCards/CreditCards.UITests/CreditCardWebAppShould.cs
+++ CreditCards/CreditCards.UITests/CreditCardWebAppShould.cs
@@ -7,26 +7,105 @@ namespace CreditCards.UITests
 {
 	public class CreditCardWebAppShould
 	{
+		#region Constants
+		private const string AboutUrl = "http://localhost:5258/Home/About";
+		private const string HomeUrl = "http://localhost:5258/";
+		private const string HomeTitle = "Home Page - Credit Cards";
+		#endregion
+
 		private static readonly string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+		private static readonly ChromeOptions options = new ChromeOptions();
+
+		public CreditCardWebAppShould()
+		{
+			options.AddArguments("no-sandbox", "disable-dev-shm-usage", "incognito");
+			options.AddExcludedArgument("enable-automation");
+			options.AddAdditionalOption("useAutomationExtension", false);
+			//if (config.Browser.IsHeadless)
+			//	options.AddArguments("--headless");
+		}
 
 		[Fact]
 		[Trait("Category", "Smoke")]
 		public void LoadApplicationPage()
 		{
-			var options = new ChromeOptions();
-			options.AddArguments("no-sandbox", "disable-dev-shm-usage", "incognito");
-			options.AddExcludedArgument("enable-automation");
-			options.AddAdditionalOption("useAutomationExtension", false);
-
-			//if (config.Browser.IsHeadless)
-			//	options.AddArguments("--headless");
-
 			using var driver = new ChromeDriver(path, options)
 			{
-				Url = "http://localhost:5258/"
+				Url = HomeUrl
 			};
 
 			driver.Manage().Window.Maximize();
+			Helper.Pause();
+
+			Assert.Equal(HomeTitle, driver.Title);
+			Assert.Equal(HomeUrl, driver.Url);
+		}
+
+		[Fact]
+		[Trait("Category", "Smoke")]
+		public void ReloadHomePage()
+		{
+			using var driver = new ChromeDriver(path, options)
+			{
+				Url = HomeUrl
+			};
+
+			driver.Manage().Window.Maximize();
+			Helper.Pause();
+
+			driver.Navigate().Refresh();
+
+			Assert.Equal(HomeTitle, driver.Title);
+			Assert.Equal(HomeUrl, driver.Url);
+		}
+
+		[Fact]
+		[Trait("Category", "Smoke")]
+		public void ReloadHomePageOnBack()
+		{
+			using var driver = new ChromeDriver(path, options)
+			{
+				Url = HomeUrl
+			};
+
+			driver.Manage().Window.Maximize();
+			Helper.Pause();
+			driver.Url = AboutUrl;
+			Helper.Pause();
+			driver.Navigate().Back(); 
+			Helper.Pause();
+
+			Assert.Equal(HomeTitle, driver.Title);
+			Assert.Equal(HomeUrl, driver.Url);
+
+			//TODO: Assert Page was reloaded
+		}
+
+		[Fact]
+		[Trait("Category", "Smoke")]
+		public void ReloadHomePageOnForward()
+		{
+			using var driver = new ChromeDriver(path, options)
+			{
+				Url = AboutUrl
+			};
+
+			driver.Manage().Window.Maximize();
+			Helper.Pause();
+
+			driver.Url = HomeUrl;
+			Helper.Pause();
+
+			driver.Navigate().Back();
+			Helper.Pause();
+
+			driver.Navigate().Forward();
+			Helper.Pause();
+
+			Assert.Equal(HomeTitle, driver.Title);
+			Assert.Equal(HomeUrl, driver.Url);
+
+			//TODO: Assert Page was reloaded
 		}
 	}
 }
---
+++ CreditCards/CreditCards.UITests/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace CreditCards.UITests
+{
+	internal static class Helper
+	{
+		public static void Pause(int ms = 2000)
+		{
+			Thread.Sleep(ms);
+		}
+	}
+}
